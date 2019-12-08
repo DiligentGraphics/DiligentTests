66ba80d3a33b18fb026e91d200578c6bec10f02c
Egor Yusov <egor.yusov@gmail.com>
2019-12-08 15:56:48 -0800

    Another experiment with ERRORLEVEL

--- run_test.bat
+++ run_test.bat
@@ -1,3 +1,6 @@
 set ERROR=0
-C:\projects\build\DiligentTests\DiligentCoreTests\%CONFIGURATION%\DiligentCore_Tests.exe || ERROR=%ERRORLEVEL%
+C:\projects\build\DiligentTests\DiligentCoreTests\%CONFIGURATION%\DiligentCore_Tests.exe
+if %ERRORLEVEL% NEQ 0 (
+	set ERROR=%ERRORLEVEL%
+)
 exit /B %ERROR%
