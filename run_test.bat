1a3061dac9244e3537ed6e3dbbf2ff02fcd2c268
assiduous <assiduous@diligentgraphics.com>
2019-12-12 16:35:25 -0800

    Testing test failure

--- run_test.bat
+++ run_test.bat
@@ -3,4 +3,6 @@ C:\projects\build\DiligentTests\DiligentCoreTests\%CONFIGURATION%\DiligentCore_T
 if %ERRORLEVEL% NEQ 0 (
 	set ERROR=%ERRORLEVEL%
 )
+
+@echo Error level : %ERRORLEVEL%
 exit /B %ERROR%
